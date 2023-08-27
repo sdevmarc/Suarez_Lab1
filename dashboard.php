45a4df62802e3ed756bbb033c82387d7bfece114
Marc Edison <suanieeee@yahoo.com>
2023-08-27 23:42:58 +0800

    echo in html seems good

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,47 @@
+<?php
+session_start();
+
+$username = $_SESSION['username'];
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="dashboard.css">
 </head>
+
 <body>
-    
+    <h1>Dashboard</h1>
+    <form action="dashboard.php" method="post">
+        <button name="logout">Logout</button>
+    </form>
+    <div class="message">Welcome back, <?= $username ?></div>
+
+
+
+
+
+
 </body>
+
 </html>
+
+<?php
+
+try {
+    if (isset($_POST['logout'])) {
+        session_unset();
+        session_destroy();
+        header('location: index.php');
+        exit();
+    }
+} catch (Exception $e) {
+    echo "Error Encountered";
+}
+?>
