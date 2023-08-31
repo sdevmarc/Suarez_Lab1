fa9720f9b231fbdfb11cdab56a62a8abd16ab13a
sdevmarc <suanieeee@yahoo.com>
2023-08-31 19:58:44 +0800

    Added Home Page

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+
+
+<header class="header">
+    <div class="logo">
+        <a href="home.php">Laza<span>marc.</span></a>
+    </div>
+</header>
+    
+</body>
+</html>
