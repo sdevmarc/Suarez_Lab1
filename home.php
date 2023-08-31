e911c821de4c2f9fba805430d41f234979012dcb
sdevmarc <suanieeee@yahoo.com>
2023-08-31 20:08:50 +0800

    Copied features from the index

--- home.php
+++ home.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
 </head>
+
 <body>
 
 
-<header class="header">
-    <div class="logo">
-        <a href="home.php">Laza<span>marc.</span></a>
-    </div>
-</header>
-    
+    <header class="header">
+        <div class="logo">
+            <a href="">Laza<span>marc.</span></a>
+        </div>
+        <div class="navBars">
+            <a href="">Men</a>
+            <a href="">Women</a>
+            <a href="">Accessories</a>
+            <a href="">About</a>
+            <button>Show Finder Quiz</button>
+        </div>
+        <div class="supNav">
+            <a href="">Support</a>
+            <i id="btnUser" class='bx bx-user' style='color:#020202'></i>
+            <i class='bx bxs-shopping-bag' style='color:#020202'></i>
+        </div>
+
+
+    </header>
+
 </body>
+
 </html>
